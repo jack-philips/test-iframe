152be39b0da63c44ee082b30d9c3667637ffa2be
jack-philips <reza.td.develop@gmail.com>
2024-05-02 12:18:27 +0330

    test

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    // Event listener to listen for messages from the iframe
-    const receiveMessage = event => {
-      // Check origin of the message for security
-      if (event.origin !== 'https://vinylunderground.co.uk/products/the-mr-k-edits-fritz-the-cat') return;
+  // useEffect(() => {
+  //   // Event listener to listen for messages from the iframe
+  //   const receiveMessage = event => {
+  //     // Check origin of the message for security
+  //     if (event.origin !== 'https://vinylunderground.co.uk/products/the-mr-k-edits-fritz-the-cat') return;
 
-      // Handle received data
-      console.log('Received data from iframe:', event.data);
-      // You can do whatever you want with the received data here
-    };
+  //     // Handle received data
+  //     console.log('Received data from iframe:', event.data);
+  //     // You can do whatever you want with the received data here
+  //   };
 
-    // Add event listener
-    window.addEventListener('message', receiveMessage);
+  //   // Add event listener
+  //   window.addEventListener('message', receiveMessage);
 
-    // Clean up function
-    return () => {
-      // Remove event listener when component unmounts
-      window.removeEventListener('message', receiveMessage);
-    };
-  }, []);
+  //   // Clean up function
+  //   return () => {
+  //     // Remove event listener when component unmounts
+  //     window.removeEventListener('message', receiveMessage);
+  //   };
+  // }, []);
 
   return (
     <div>
